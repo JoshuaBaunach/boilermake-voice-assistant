a422c896f303d9b6a0494cb40f580ca3e2816131
Eric Chheang <42673389+EricChh20@users.noreply.github.com>
2018-10-20 09:52:45 -0400

    Create emailBotTrain.py

---
+++ emailBotTrain.py
@@ -0,0 +1,23 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+def setup():
+    chatbot = ChatBot('Bot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    trainer='chatterbot.trainers.ListTrainer')
+
+    for file in os.listdir('C:/Users/tmanc/OneDrive/ML\chatterbot-corpus-master/chatterbot_corpus/data/email/'):
+
+        convData = open(r'C:/Users/tmanc/OneDrive/ML\chatterbot-corpus-master/chatterbot_corpus/data/email/' + file,encoding='latin-1').readlines()
+        chatbot.set_trainer(ListTrainer)
+        chatbot.train(convData)
+
+
+
+
+
+
+
+
+setup()
