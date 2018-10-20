3b78284880c82604d9175cf6ee1e0fe1aba372f3
Eric Chheang <42673389+EricChh20@users.noreply.github.com>
2018-10-20 10:11:11 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,22 @@
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket 
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 from emailBot import get_response
-
 
 
 class ChatServer(WebSocket):
 
     def handleMessage(self):
-        # echos messages back to client 
-        message= self.data 
+        # echo message back to client
+        message = self.data
         response = get_response(message)
         self.sendMessage(response)
 
-
-    def handConnected(self):
+    def handleConnected(self):
         print(self.address, 'connected')
-
 
     def handleClose(self):
         print(self.address, 'closed')
 
 
 
-
-
-    #server = SimpleWebSocketServer('', 8000, ChatServer)
-    #server.serveforever()
+server = SimpleWebSocketServer('', 8000, ChatServer)
+server.serveforever()
