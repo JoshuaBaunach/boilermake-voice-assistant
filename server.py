aeb68854f8595e9bd974266b561e6ae796c37fd1
Eric Chheang <et.chheang@gmail.com>
2018-10-20 09:51:00 -0400

    emailBot.py

---
+++ server.py
@@ -0,0 +1,27 @@
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket 
+from emailBot import get_response
+
+
+
+class ChatServer(WebSocket):
+
+    def handleMessage(self):
+        # echos messages back to client 
+        message= self.data 
+        response = get_response(message)
+        self.sendMessage(response)
+
+
+    def handConnected(self):
+        print(self.address, 'connected')
+
+
+    def handleClose(self):
+        print(self.address, 'closed')
+
+
+
+
+
+    #server = SimpleWebSocketServer('', 8000, ChatServer)
+    #server.serveforever()
