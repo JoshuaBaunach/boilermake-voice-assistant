2480886f03aa2ecc96bc098111e589b8417bd19c
Eric Chheang <42673389+EricChh20@users.noreply.github.com>
2018-10-20 10:40:05 -0400

    Update emailBot.py

--- emailBot.py
+++ emailBot.py
@@ -1,32 +1,26 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer 
-
-
-bot = ChatBot('EmailBot')
-bot.set_trainer(ListTrainer)
+from chatterbot.trainers import ListTrainer
 
 
 def get_response(userText):
     bot = ChatBot('Bot',
-            storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters = [
+                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch'
-            },
+        },
         {
             'import_path': 'chatterbot.logic.LowConfidenceAdapter',
             'threshold': 0.70,
-            'default_response': 'I am sory, but I dont not understand.'
-            }
-        ],
-    trainer ='chatterbot.trainers.ListTrainer')
+            'default_response': 'I am sorry, but I do not understand.'
+        }
+    ],
+    trainer='chatterbot.trainers.ListTrainer')
     bot.set_trainer(ListTrainer)
-
     while True:
-
-        if userText.strip() != 'Bye':
-            result = bot.get_response(userText)
-            reply = bot.get_response(message)
+        if userText.strip()!= 'Bye':
+            result = bot.get_response(userText)                        
+            reply = str(result)
             return(reply)
         if userText.strip() == 'Bye':
             return('Bye')
